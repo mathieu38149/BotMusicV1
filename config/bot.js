76beb6fab2befcd0f392280c1ff0785e7cb8738b
mathieu38149 <83405336+mathieu38149@users.noreply.github.com>
2021-04-29 19:23:39 +0200

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: '',
+        prefix: '.',
+        activity: 'Fait de la musique',
+    },
+
+    filters: ['bientot'],
+};
